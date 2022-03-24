b83f7c67b97eba2938c7453acee2ca057826264b
CongSang <honguyencongsang723@gmail.com>
2022-03-24 11:06:01 +0700

    pull git

--- src/main/java/com/ou/oubusmanager/TripManageController.java
+++ src/main/java/com/ou/oubusmanager/TripManageController.java
@@ -3,12 +3,7 @@ package com.ou.oubusmanager;
 import com.ou.pojo.Admin;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.*;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -17,49 +12,7 @@ import javafx.scene.input.MouseEvent;
  */
 public class TripManageController implements Initializable {
     
-    @FXML
-    private Button btnBookTicket;
-
-    @FXML
-    private ImageView btnLogout;
-
-    @FXML
-    private DatePicker dpDateDeparture;
-
-    @FXML
-    private DatePicker dpSearchDate;
-
-    @FXML
-    private TextField dpTimeDeparture;
-
-    @FXML
-    private Label lblCustomerName;
-
-    @FXML
-    private ListView<?> lvTrip;
-
-    @FXML
-    private TextField txtFrom;
-
-    @FXML
-    private TextField txtSearchFrom;
-
-    @FXML
-    private TextField txtSearchTo;
-
-    @FXML
-    private TextField txtTo;
     private Admin admin;
-
-    @FXML
-    void btnBookTicketClick(ActionEvent event) {
-
-    }
-
-    @FXML
-    void btnLogoutClick(MouseEvent event) {
-
-    }
 
     public void setAdmin(Admin admin) {
         this.admin = admin;
